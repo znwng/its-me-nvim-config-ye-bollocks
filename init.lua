48810989ec76187cbd292a43c5742ed82586a113
znwng <kalyan3x90@gmail.com>
2025-08-04 05:57:00 +0530

    added a drop off where you left function

--- init.lua
+++ init.lua
@@ -74,3 +74,15 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank({ timeout = 200 })
 	end,
 })
+
+-- Drop off at last cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = "*",
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			vim.api.nvim_win_set_cursor(0, mark)
+		end
+	end,
+})
