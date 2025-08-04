ab6a868fcc31b176762e49b66331d61003df0e9d
znwng <kalyan3x90@gmail.com>
2025-08-04 14:25:06 +0530

    disabled swapfile and backup and set to tabs

--- init.lua
+++ init.lua
@@ -4,10 +4,26 @@ vim.opt.colorcolumn = "100"
 vim.opt.signcolumn = "yes"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
 vim.o.mouse = "a"
+
+-- Tabs
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+-- idk what to name this section
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 
 -- Transparent background
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
