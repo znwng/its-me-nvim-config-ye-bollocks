18189dbdd027760201718a7d26dd606d98093413
znwng <kalyan3x90@gmail.com>
2025-08-05 09:02:09 +0530

    ignore

--- init.lua
+++ init.lua
@@ -1,35 +1,39 @@
 -- UI settings
 
-vim.opt.termguicolors = true -- Enable 24-bit RGB color in the terminal
-vim.opt.colorcolumn = "100" -- Show a vertical line column (visual guide)
-vim.opt.signcolumn = "yes" -- Always show the sign column (for git/LSP signs etc.)
-vim.opt.number = true -- Show absolute line number on the current line
-vim.opt.relativenumber = true -- Show relative line numbers on all other lines
-vim.opt.scrolloff = 10 -- Keep 10 lines above/below cursor when scrolling
-vim.o.mouse = "a" -- Enable mouse support in all modes
+vim.opt.termguicolors = true
+vim.opt.colorcolumn = "100"
+vim.opt.signcolumn = "yes"
+vim.opt.number = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 10
+vim.o.mouse = "a"
 
 -- Tab and indentation behavior
 
-vim.opt.expandtab = true -- Convert tabs to spaces
-vim.opt.tabstop = 4 -- Number of spaces a <Tab> equals to
-vim.opt.softtabstop = 4 -- Number of spaces used while editing
-vim.opt.shiftwidth = 4 -- Number of spaces used for each level of indentation
-vim.opt.smartindent = true -- Smart autoindentation for new lines
-vim.opt.autoindent = true -- Copy indentation from current line when starting a new one
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 -- File history and backups
 
-vim.opt.swapfile = false -- Don't create a swap file (tmp editing file)
-vim.opt.backup = false -- Don't create backup files
-vim.opt.undofile = true -- Enable persistent undo (survives file close)
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- Set undo history directory
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 
 -- Transparent background highlights
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- Make main editing area transparent
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" }) -- Make inactive window transparent
-vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" }) -- Transparent popup menu
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" }) -- Transparent floating window borders
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 
 -- Statusline highlight groups
 
@@ -47,12 +51,12 @@ function _G.diag_count(severity)
 end
 
 -- Custom statusline layout
-vim.opt.statusline = "%{expand('%:p')} %m %=" -- Full file path + modified flag
-	.. "%#StatusLineError#E:%{v:lua.diag_count('Error')} " -- Error count (red)
-	.. "%#StatusLineWarn#W:%{v:lua.diag_count('Warn')} " -- Warning count (yellow)
-	.. "%#StatusLineHint#H:%{v:lua.diag_count('Hint')} " -- Hint count (teal)
-	.. "%#StatusLineInfo#I:%{v:lua.diag_count('Info')} " -- Info count (blue)
-	.. "%#StatusLine#[%l:%c]" -- Cursor position [line:col]
+vim.opt.statusline = "%{expand('%:p')} %m %="
+	.. "%#StatusLineError#E:%{v:lua.diag_count('Error')} "
+	.. "%#StatusLineWarn#W:%{v:lua.diag_count('Warn')} "
+	.. "%#StatusLineHint#H:%{v:lua.diag_count('Hint')} "
+	.. "%#StatusLineInfo#I:%{v:lua.diag_count('Info')} "
+	.. "%#StatusLine#[%l:%c]"
 
 -- Load plugins from 'config.lazy' module
 require("config.lazy")
@@ -82,10 +86,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.api.nvim_create_autocmd("BufReadPost", {
 	pattern = "*",
 	callback = function()
-		local mark = vim.api.nvim_buf_get_mark(0, '"') -- Last cursor mark
-		local lcount = vim.api.nvim_buf_line_count(0) -- Total lines in file
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
 		if mark[1] > 0 and mark[1] <= lcount then
-			vim.api.nvim_win_set_cursor(0, mark) -- Restore position if valid
+			vim.api.nvim_win_set_cursor(0, mark)
 		end
 	end,
 })
