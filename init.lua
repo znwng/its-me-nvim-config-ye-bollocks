be2566841587bbf0f0fa81453cb471d228253506
znwng <kalyan3x90@gmail.com>
2025-08-07 00:48:05 +0530

    added auto-format on save

--- init.lua
+++ init.lua
@@ -94,3 +94,11 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	end,
 })
 
+-- Auto-format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		vim.lsp.buf.format({ async = false })
+	end,
+})
+
