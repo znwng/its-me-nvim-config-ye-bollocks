feadd4f3039b7f7a65832163cd2dc385306117b4
znwng <kalyan3x90@gmail.com>
2025-08-08 18:35:19 +0530

    stuff

--- init.lua
+++ init.lua
@@ -41,59 +41,69 @@ vim.api.nvim_set_hl(0, "StatusLineInfo", { bg = "NONE", fg = "#31748f" })
 
 -- Function: count diagnostics of a given severity for current buffer
 function _G.diag_count(severity)
-    local diags = vim.diagnostic.get(0, { severity = vim.diagnostic.severity[severity:upper()] })
-    return #diags
+	local diags = vim.diagnostic.get(0, { severity = vim.diagnostic.severity[severity:upper()] })
+	return #diags
 end
 
 -- Custom statusline layout
 vim.opt.statusline = "%{expand('%:p')} %m %="
-    .. "%#StatusLineError#E:%{v:lua.diag_count('Error')} "
-    .. "%#StatusLineWarn#W:%{v:lua.diag_count('Warn')} "
-    .. "%#StatusLineHint#H:%{v:lua.diag_count('Hint')} "
-    .. "%#StatusLineInfo#I:%{v:lua.diag_count('Info')} "
-    .. "%#StatusLine#[%l:%c]"
+	.. "%#StatusLineError#E:%{v:lua.diag_count('Error')} "
+	.. "%#StatusLineWarn#W:%{v:lua.diag_count('Warn')} "
+	.. "%#StatusLineHint#H:%{v:lua.diag_count('Hint')} "
+	.. "%#StatusLineInfo#I:%{v:lua.diag_count('Info')} "
+	.. "%#StatusLine#[%l:%c]"
 
 -- Load plugins from 'config.lazy' module
 require("config.lazy")
 
 -- Automatically restore transparency when colorscheme is changed
 vim.api.nvim_create_autocmd("ColorScheme", {
-    pattern = "*",
-    callback = function()
-        vim.api.nvim_set_hl(0, "CmpBorder", { bg = "none" })
-        vim.api.nvim_set_hl(0, "CmpDocBorder", { bg = "none" })
-        vim.api.nvim_set_hl(0, "CmpDoc", { bg = "none" })
-        vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
-        vim.api.nvim_set_hl(0, "PmenuSel", { bg = "none", blend = 0 })
-        vim.api.nvim_set_hl(0, "PmenuBorder", { bg = "none" })
-    end,
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_set_hl(0, "CmpBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "CmpDocBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "CmpDoc", { bg = "none" })
+		vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+		vim.api.nvim_set_hl(0, "PmenuSel", { bg = "none", blend = 0 })
+		vim.api.nvim_set_hl(0, "PmenuBorder", { bg = "none" })
+	end,
 })
 
 -- Highlight yanked text briefly (like VSCode flash)
 vim.api.nvim_create_autocmd("TextYankPost", {
-    pattern = "*",
-    callback = function()
-        vim.highlight.on_yank({ timeout = 200 })
-    end,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ timeout = 200 })
+	end,
 })
 
 -- Restore cursor to last position when reopening a file
 vim.api.nvim_create_autocmd("BufReadPost", {
-    pattern = "*",
-    callback = function()
-        local mark = vim.api.nvim_buf_get_mark(0, '"')
-        local lcount = vim.api.nvim_buf_line_count(0)
-        if mark[1] > 0 and mark[1] <= lcount then
-            vim.api.nvim_win_set_cursor(0, mark)
-        end
-    end,
+	pattern = "*",
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			vim.api.nvim_win_set_cursor(0, mark)
+		end
+	end,
 })
 
--- Convert leading spaces to tabs automatically on save
+-- Autoformat before save
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function()
-        vim.cmd("retab!")
-    end,
+	pattern = "*",
+	callback = function()
+		vim.lsp.buf.format({ async = false })
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function()
+		local last_line = vim.api.nvim_buf_get_lines(0, -2, -1, false)[1]
+		if last_line ~= "" then
+			vim.api.nvim_buf_set_lines(0, -1, -1, false, { "" })
+		end
+	end,
 })
 
