16c975c873d2d4c6ffd4969f1a3e36820aa1ffeb
znwng <kalyan3x90@gmail.com>
2025-08-13 06:06:22 +0530

    diagnostics squiggle lines are not straight

--- lua/plugins/rose_pine.lua
+++ lua/plugins/rose_pine.lua
@@ -1,20 +1,28 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	lazy = false,
-	priority = 1000,
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
 
-	config = function()
-		require("rose-pine").setup({
-			variant = "moon", -- options: 'main' (default), 'moon', 'dawn'
-			disable_background = true,
-			styles = {
-				italic = false,
-				bold = false,
-			},
-		})
+    config = function()
+        require("rose-pine").setup({
+            variant = "moon", -- options: 'main' (default), 'moon', 'dawn'
+            disable_background = true,
+            styles = {
+                italic = false,
+                bold = false,
+            },
+        })
 
-		vim.cmd.colorscheme("rose-pine")
-	end,
+        vim.cmd.colorscheme("rose-pine")
+
+        -- This has to be *inside* config, after colorscheme is set
+        vim.cmd([[
+          highlight DiagnosticUnderlineError gui=underline cterm=underline
+          highlight DiagnosticUnderlineWarn gui=underline cterm=underline
+          highlight DiagnosticUnderlineInfo gui=underline cterm=underline
+          highlight DiagnosticUnderlineHint gui=underline cterm=underline
+        ]])
+    end,
 }
 
