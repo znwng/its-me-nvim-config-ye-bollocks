e247d566ef5a0d16e92c5ca70ffdc97940422663
znwng <kalyan3x90@gmail.com>
2025-10-12 16:40:14 +0530

    Removed auto-format on save

--- init.lua
+++ init.lua
@@ -120,15 +120,6 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	end,
 })
 
--- Auto-format before save
-vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function()
-		if vim.lsp.buf.format then
-			pcall(vim.lsp.buf.format, { async = false })
-		end
-	end,
-})
-
 -- Ensure file ends with newline
 vim.api.nvim_create_autocmd("BufWritePre", {
 	callback = function()
