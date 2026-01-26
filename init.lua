f8311b6158622c18f2d3786ea30c627487434795
znwng <kalyan3x90@gmail.com>
2026-01-27 02:52:36 +0530

    using system clipboard as default register

--- init.lua
+++ init.lua
@@ -20,6 +20,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
 vim.opt.showmode = false
+vim.opt.clipboard = "unnamedplus"
 
 -- Indentation / Tabs
 vim.opt.expandtab = false
