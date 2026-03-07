ac1e10ba138f138594a7d0fa61eb18737070470d
znwng <kalyan3x90@gmail.com>
2026-03-07 22:04:33 +0530

    add oil.nvim and remove netrw dependance

--- lua/plugins/indentblankline.lua
+++ lua/plugins/indentblankline.lua
@@ -2,9 +2,14 @@ return {
     {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
-        ---@module "ibl"
-        ---@type ibl.config
-        opts = {},
+        opts = {
+            indent = {
+                char = "│",
+            },
+            scope = {
+                enabled = false,
+            },
+        },
     },
 }
 
---
+++ lua/plugins/oil.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "stevearc/oil.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+
+        opts = {
+            view_options = {
+                show_hidden = true,
+            },
+        },
+
+        keys = {
+            {
+                "<leader>e",
+                "<cmd>Oil<CR>",
+                desc = "Open file explorer",
+            },
+        },
+    },
+}
+
