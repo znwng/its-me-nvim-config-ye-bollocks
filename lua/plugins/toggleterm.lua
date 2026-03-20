94fbc053d23aec5a3ef9f6e60fc3ca9b391bf3fe
znwng <kalyan3x90@gmail.com>
2026-03-20 15:01:34 +0530

    added toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        config = function()
+            require("toggleterm").setup({
+                direction = "float",
+                -- direction = "horizontal",
+                -- size = 10,
+            })
+        end,
+
+        vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<CR>"),
+    },
+}
+
