d050c5bcc19e6f44df87f9e666381e4e618b3075
znwng <kalyan3x90@gmail.com>
2026-03-25 01:23:57 +0530

    added fidget and chagend key bind for oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -11,7 +11,7 @@ return {
         },
         keys = {
             {
-                "<leader>e",
+                "<leader>o",
                 "<cmd>Oil<CR>",
                 desc = "Open file explorer",
             },
