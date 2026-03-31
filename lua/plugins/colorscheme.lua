a19dd8a25522f959834be346541a06de0dfe9728
znwng <kalyan3x90@gmail.com>
2026-03-31 15:48:10 +0530

    changed colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,29 +1,26 @@
-return {
-    "wtfox/jellybeans.nvim",
+return -- Using lazy.nvim
+{
+    "metalelf0/black-metal-theme-neovim",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd("colorscheme jellybeans")
+        require("black-metal").setup({
+            -- [ bathory burzum dark-funeral darkthrone emperor gorgoroth immortal
+            --  impaled-nazarene khold marduk mayhem nile taake thyrfing venom windir]
+            theme = "windir",
+        })
+        require("black-metal").load()
+
         local function apply_ui()
-            vim.api.nvim_set_hl(0, "StatusLineError", { fg = "#d74545", bg = "NONE", bold = true })
-            vim.api.nvim_set_hl(0, "StatusLineWarn", { fg = "#ffaf00", bg = "NONE", bold = true })
-            vim.api.nvim_set_hl(0, "StatusLineHint", { fg = "#a08070", bg = "NONE", bold = true })
-            vim.api.nvim_set_hl(0, "StatusLineInfo", { fg = "#5f9ecf", bg = "NONE", bold = true })
-            vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { underline = true, sp = "#d74545" })
-            vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { underline = true, sp = "#ffaf00" })
-            vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { underline = true, sp = "#a08070" })
-            vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { underline = true, sp = "#5f9ecf" })
-            vim.api.nvim_set_hl(0, "StatusLineMode", { fg = "#9ccfd8", bg = "#2a2a2b" })
+            vim.api.nvim_set_hl(0, "StatusLineError", { fg = "#d74545", bg = "NONE" })
+            vim.api.nvim_set_hl(0, "StatusLineWarn", { fg = "#ffaf00", bg = "NONE" })
+            vim.api.nvim_set_hl(0, "StatusLineHint", { fg = "#a08070", bg = "NONE" })
+            vim.api.nvim_set_hl(0, "StatusLineInfo", { fg = "#5f9ecf", bg = "NONE" })
+            vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { underline = true })
+            vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { underline = true })
+            vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { underline = true })
+            vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { underline = true })
             vim.api.nvim_set_hl(0, "colorColumn", { bg = "#2a2b2b" })
-            vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "VertSplit", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "TabLine", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "TabLineFill", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "TabLineSel", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#2a2a2b" })
-            vim.api.nvim_set_hl(0, "IblIndent", { fg = "#252525", nocombine = true })
         end
         apply_ui()
         vim.api.nvim_create_autocmd("ColorScheme", {
