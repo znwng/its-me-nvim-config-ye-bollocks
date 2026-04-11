34a88adae1185c9e2679a9b19cf6f9ecf211642f
znwng <kalyan3x90@gmail.com>
2026-04-12 01:17:26 +0530

    state-0003

--- lua/plugins/indentblankline.lua
+++ lua/plugins/indentblankline.lua
@@ -3,7 +3,8 @@ return {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
         opts = {
-            indent = { char = "┆" },
+            -- indent = { char = "┆" },
+            indent = { char = "│" },
             scope = { enabled = false },
         },
     },
