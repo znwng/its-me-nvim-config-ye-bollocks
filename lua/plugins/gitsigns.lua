1301c4ee2f8c0680017da4b3de37715ba61182c8
znwng <kalyan3x90@gmail.com>
2026-04-17 06:35:52 +0530

    state-0012

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -35,23 +35,32 @@ return {
         "lewis6991/gitsigns.nvim",
         config = function()
             vim.g.mapleader = " "
+
             require("gitsigns").setup({
                 attach_to_untracked = false,
-                current_line_blame = true,
+
+                -- Disabled by default (you can toggle with <leader>tb)
+                current_line_blame = false,
+
                 current_line_blame_opts = {
                     virt_text = true,
                     virt_text_pos = "eol",
                     delay = 250,
                     ignore_whitespace = false,
                 },
+
                 current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+
                 on_attach = function(bufnr)
                     local gs = require("gitsigns")
+
                     local function map(mode, lhs, rhs, opts)
                         opts = opts or {}
                         opts.buffer = bufnr
                         vim.keymap.set(mode, lhs, rhs, opts)
                     end
+
+                    -- Navigation
                     map("n", "]c", function()
                         if vim.wo.diff then
                             vim.cmd.normal({ "]c", bang = true })
@@ -59,6 +68,7 @@ return {
                             gs.nav_hunk("next")
                         end
                     end)
+
                     map("n", "[c", function()
                         if vim.wo.diff then
                             vim.cmd.normal({ "[c", bang = true })
@@ -66,32 +76,50 @@ return {
                             gs.nav_hunk("prev")
                         end
                     end)
+
+                    -- Hunk actions
                     map("n", "<leader>hs", gs.stage_hunk)
                     map("n", "<leader>hr", gs.reset_hunk)
+
                     map("v", "<leader>hs", function()
                         gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
                     end)
+
                     map("v", "<leader>hr", function()
                         gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
                     end)
+
                     map("n", "<leader>hS", gs.stage_buffer)
                     map("n", "<leader>hR", gs.reset_buffer)
+
+                    -- Preview
                     map("n", "<leader>hp", gs.preview_hunk)
                     map("n", "<leader>hi", gs.preview_hunk_inline)
+
+                    -- Blame & diff
                     map("n", "<leader>hb", function()
                         gs.blame_line({ full = true })
                     end)
+
                     map("n", "<leader>hd", gs.diffthis)
+
                     map("n", "<leader>hD", function()
                         gs.diffthis("HEAD~")
                     end)
+
+                    -- Quickfix
                     map("n", "<leader>hQ", function()
                         gs.setqflist("all")
                     end)
+
                     map("n", "<leader>hq", gs.setqflist)
+
+                    -- Toggles
                     map("n", "<leader>tb", gs.toggle_current_line_blame)
                     map("n", "<leader>td", gs.toggle_deleted)
                     map("n", "<leader>tw", gs.toggle_word_diff)
+
+                    -- Text object
                     map({ "o", "x" }, "ih", gs.select_hunk)
                 end,
             })
