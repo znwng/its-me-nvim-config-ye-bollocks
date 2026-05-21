7e31dd921186d46067966a86bbb20d14efe1eed1
znwng <kalyan3x90@gmail.com>
2026-05-21 20:58:34 +0530

    state-0020

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,7 +1,11 @@
 return {
     "j-hui/fidget.nvim",
     opts = {
-        -- options
+        notification = {
+            window = {
+                winblend = 0, -- 0 = fully opaque
+            },
+        },
     },
 }
 
