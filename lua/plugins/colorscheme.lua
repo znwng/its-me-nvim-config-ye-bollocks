8ba41daad356832c8a335be01a239e24d01bbbc3
znwng <kalyan3x90@gmail.com>
2026-05-21 19:50:24 +0530

    state-0019

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -16,23 +16,25 @@ return {
 
         -- KEEP your original UI exactly unchanged
         local function apply_ui()
-            vim.api.nvim_set_hl(0, "StatusLine", { bg = "#101010" })
-            vim.api.nvim_set_hl(0, "StatusLinePath", { fg = "#c7c7c7", bg = "#101010" })
-            vim.api.nvim_set_hl(0, "StatusLineMode", { fg = "#6e94b2", bg = "#101010" })
-            vim.api.nvim_set_hl(0, "StatusLineBranch", { fg = "#c7c7c7", bg = "#101010" })
-            vim.api.nvim_set_hl(0, "StatusLineCur", { fg = "#808080", bg = "#101010" })
-            vim.api.nvim_set_hl(0, "StatusLineLines", { fg = "#808080", bg = "#101010" })
-            vim.api.nvim_set_hl(0, "StatusLineError", { fg = "#d74545", bg = "#101010" })
-            vim.api.nvim_set_hl(0, "StatusLineWarn", { fg = "#ffaf00", bg = "#101010" })
-            vim.api.nvim_set_hl(0, "StatusLineHint", { fg = "#a08070", bg = "#101010" })
-            vim.api.nvim_set_hl(0, "StatusLineInfo", { fg = "#5f9ecf", bg = "#101010" })
+            -- local bg = "#101010"
+            local bg = "#222222"
+            vim.api.nvim_set_hl(0, "StatusLine", { bg = bg })
+            vim.api.nvim_set_hl(0, "StatusLinePath", { fg = "#c7c7c7", bg = bg })
+            vim.api.nvim_set_hl(0, "StatusLineMode", { fg = "#6e94b2", bg = bg })
+            vim.api.nvim_set_hl(0, "StatusLineBranch", { fg = "#c7c7c7", bg = bg })
+            vim.api.nvim_set_hl(0, "StatusLineCur", { fg = "#808080", bg = bg })
+            vim.api.nvim_set_hl(0, "StatusLineLines", { fg = "#808080", bg = bg })
+            vim.api.nvim_set_hl(0, "StatusLineError", { fg = "#d74545", bg = bg })
+            vim.api.nvim_set_hl(0, "StatusLineWarn", { fg = "#ffaf00", bg = bg })
+            vim.api.nvim_set_hl(0, "StatusLineHint", { fg = "#a08070", bg = bg })
+            vim.api.nvim_set_hl(0, "StatusLineInfo", { fg = "#5f9ecf", bg = bg })
 
             vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { underline = true })
             vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { underline = true })
             vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { underline = true })
             vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { underline = true })
 
-            vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#101010" })
+            vim.api.nvim_set_hl(0, "ColorColumn", { bg = bg})
         end
 
         apply_ui()
